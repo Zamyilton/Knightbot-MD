4c17a50de689833502d8021769fd10e5216a86cc
Isaac Gold <102518035+Zamyilton@users.noreply.github.com>
2025-05-29 22:04:38 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'VALEN',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "VALEN",
+  botOwner: 'Lord Zamyilton', // Your name
+  ownerNumber: '2348053107478', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
+  description: "VALEN is shadow garden's core.",
   version: "2.0.1",
 };
 
